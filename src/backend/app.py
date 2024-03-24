f4458a7de0d7bd05e71239a686d8b73c0476bb1a
Calinciuc Laura <calinciuclaura@gmail.com>
2024-03-24 16:06:01 +0200

    Add database connection and insertion functionality

--- src/backend/app.py
+++ src/backend/app.py
@@ -2,10 +2,24 @@ from flask import Flask, render_template, current_app, send_file
 from ColorPicker import color_pallete
 from ChatBot import chatBot
 from flask_cors import CORS
-from flask import request, jsonify
+from flask import request, jsonify, g
+from database import create_connection, create_table, close_connection, insert_into_database
 
 app = Flask(__name__, static_folder='images', static_url_path='/')
 CORS(app)
+
+
+@app.before_request
+def before_request():
+    g.db, g.cursor = create_connection()
+    create_table(g.cursor)
+
+
+@app.teardown_appcontext
+def teardown(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        close_connection(*db)
 
 
 @app.route('/')
@@ -20,6 +34,11 @@ def chatgpt():
     result = chatBot(text)
     if result is None:
         result = "No result from chatBot"
+
+    g.db, g.cursor = create_connection()
+    insert_into_database(g.cursor, result)
+    g.db.commit()
+
     return jsonify(result)
 
 
--- src/backend/database.py
+++ src/backend/database.py
@@ -1,16 +1,27 @@
 import sqlite3
 
-connection = sqlite3.connect("app.db")
-print(connection.total_changes)
-cursor = connection.cursor()
-cursor.execute(
-    "CREATE TABLE IF NOT EXISTS session (id INTEGER PRIMARY KEY AUTOINCREMENT, prompt TEXT)")
-cursor.execute("INSERT INTO session VALUES (1, 'alice')")
-cursor.execute("INSERT INTO session VALUES (2, 'bob')")
-cursor.execute("INSERT INTO session VALUES (3, 'eve')")
-connection.commit()
 
-cursor.execute("SELECT * FROM example")
-print(cursor.fetchall())
-cursor.close()
-connection.close()
+def create_connection():
+    connection = sqlite3.connect("app.db")
+    cursor = connection.cursor()
+    return connection, cursor
+
+
+def create_table(cursor):
+    cursor.execute(
+        "CREATE TABLE IF NOT EXISTS session (id INTEGER PRIMARY KEY AUTOINCREMENT, prompt TEXT)"
+    )
+
+
+def close_connection(connection, cursor):
+    cursor.close()
+    connection.close()
+
+
+def insert_into_database(cursor, text):
+    cursor.execute(f"INSERT INTO session (prompt) VALUES  (?)", (text,))
+
+
+def fetch_all(cursor):
+    cursor.execute("SELECT * FROM session")
+    return cursor.fetchall()
